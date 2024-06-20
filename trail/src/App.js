ed42308478073845ef08d157dae64beb76490a48
harshasamavedam <samavedam1999@gmail.com>
2024-06-20 20:24:17 +0530

    LoginForm created

---
+++ trail/src/App.js
@@ -0,0 +1,8 @@
+import Loginform from "./loginforms/Primary"
+import './App.css';
+
+function App() {
+  return <Loginform/>
+}
+
+export default App;
---
+++ trail/src/loginforms/Primary.js
@@ -0,0 +1,19 @@
+import "./Loginstyle.css"
+
+
+
+export default function Loginform(){
+
+return (
+    <div className='div1'>
+<div className='div2' >
+        <p>UserName</p>
+        <input type="text" placeholder="Enter username" />
+        <p>passwpord</p>
+        <input type="password" placeholder="enter your password"/>
+        <p>
+            Login as user
+            </p>
+        </div>
+        </div>)
+}
