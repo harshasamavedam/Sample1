5519e317e6e063e7759ffc9d2cb0a97398b62032
harshasamavedam <samavedam1999@gmail.com>
2024-06-25 22:48:00 +0530

    updated login form with condition

--- trail/src/loginforms/Primary.js
+++ trail/src/loginforms/Primary.js
@@ -1,39 +1,34 @@
+import {useState} from "react"
+import {redirect} from 'react-router-dom'
+import Home from './Home/Home' 
 import "./Loginstyle.css"
 
-let username=""
-let password=""
-
-
-function click(event){
-    event.preventDefault()
-    console.log(username,password)
-    console.log(event.target)
-    console.log("entered some stuff")
-    username=""
-    password=""
-    return <h1> Done</h1>
-}
-
-
-function userup(event){
-    username=event.target.value
-    console.log(username)
-}
-function passchange(event){
-    password=event.target.value
-    console.log(password)
-}
-
 export default function Loginform(){
+let [username,userUp]=useState("")
+let [password,passChange]=useState("")
+let[login,Change]=useState(false)
 
-return (
-    <div className='div1'>
+
+const logout=()=>{
+
+    return <div>
+        <p>done entered</p>
+        <button onClick={()=>Change(false)}>logout</button>
+    </div>
+}
+
+const opt=()=>{
+    return <div className='div1'>
 <div className='div2' >
     <form onSubmit={click}>
+
+
         <p>UserName</p>
-        <input type="text" placeholder="Enter username" id="User" value={username} onChange={userup}/>
-        <p>passwpord</p>
-        <input type="password" placeholder="enter your password" id="pass" value={password}  onChange={passchange}/>
+        <input type="text" placeholder="Enter username" id="User"  value={username} onChange={(event)=>{
+            console.log(event.target.value)
+            userUp(event.target.value)}}/>
+        <p>Passwpord</p>
+        <input type="password" placeholder="enter your password" id="pass" value={password} onChange={(event)=>passChange(event.target.value)}/>
         <br/>
         <br/>
         <button type="submit">
@@ -42,5 +37,16 @@ return (
             </form>
 
         </div>
-        </div>)
+        </div>
+}
+
+const click=(event)=>{
+    event.preventDefault()
+    userUp(()=>"")
+    passChange(()=>"")
+    console.log(event)
+    Change(()=>true)
+    }
+console.log(login)
+return login? logout():opt()
 }
